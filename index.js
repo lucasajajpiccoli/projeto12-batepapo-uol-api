6c8b16304b250517c56edd8e911bb86e0cfb3e07
lucasajajpiccoli <lucasajaj@gmail.com>
2022-09-04 05:40:31 -0300

    Rota /participants, método POST

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'corts';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { MongoClient, ObjectId } from 'mongodb';
 import joi from 'joi';
@@ -14,5 +14,51 @@ dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect().then(() => { db = mongoClient.db('test') });
+
+const participantSchema = joi.object({
+    name: joi.string().empty().required()
+});
+
+const messageSchema = joi.object({
+    to: joi.string().empty().required(),
+    text: joi.string().empty().required(),
+    type: joi.valid('message', 'private_message').required()
+});
+
+function doStatusMessage (participant, status) {
+    const actualTime = dayjs().format('HH:mm:ss');
+    const statusMessage = {
+        from: participant.name,
+        to: 'Todos',
+        text: `${status} sala...`,
+        type: 'status',
+        time: actualTime
+    };
+    return statusMessage;
+}
+
+app.post('/participants', async (request, response) => {
+    const validation = participantSchema.validate(request.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        response.status(422).send(errors);
+        return;
+    }
+
+    try {
+        const isThereParticipant = await db.collection('participants').findOne({name: request.body.name});
+        if (isThereParticipant) {
+            response.sendStatus(409);
+            return;
+        }
+        const participant = {name: request.body.name, lastStatus: Date.now()};
+        await db.collection('participants').insertOne(participant);
+        const statusMessage = doStatusMessage(participant, 'entra na');
+        await db.collection('messages').insertOne(statusMessage);
+        response.sendStatus(201);
+    } catch (error) {
+        response.status(500).send(error.message);
+    }
+});
 
 app.listen(5000, () => console.log("Listening on port 5000"));
